fa211f9e323dfa80f79a64b636c5e6afce964f64
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-02-20 20:40:56 +0100

    refactor signin page layout and update Vercel configuration for rewrites

--- src/pages/(auth)/signin.tsx
+++ src/pages/(auth)/signin.tsx
@@ -71,19 +71,7 @@ const Signin = () => {
                   </button>
                 </div>
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      onChange={handleChange}
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                </div>
+              <div className="flex items-center justify-end">
                 <a
                   href="#"
                   className="text-sm font-medium text-brand-primary hover:underline dark:text-primary-500"
